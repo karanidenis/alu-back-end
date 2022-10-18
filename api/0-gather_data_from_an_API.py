a32733ce4faea6aabaaeb079bd10c89d30b325ab
karanidenis <you@example.com>
2022-10-18 10:45:08 +0300

    getting information from an api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,7 +3,9 @@ import requests
 import sys
 
 api_url = 'https://jsonplaceholder.typicode.com/todos/'.format(sys.argv[1])
+# this is the api url used to get the list of tasks completed 
 api_url_2 = 'https://jsonplaceholder.typicode.com/{}/'.format(sys.argv[1])
+# this url is used to get names of employees
 names_and_tasks = requests.get(api_url)
 result = names_and_tasks.json()
 titles = requests.get(api_url_2)
@@ -16,11 +18,14 @@ count_1 = 0
 
 for value in result:
     if value.get('userId') == int(sys.argv[1]):
+        # getting the total tasks supposed to be done
         count_1 += 1
     if value.get('completed') and value.get('userId') == int(sys.argv[1]):
+        # getting tasks completed
         count += 1 
     print('Employee', empl_name 'is done with tasks(', count_1'/'count '):')
     
 for value in result:
     if value.get('completed') and value.get('userId') == int(sys.argv[1]):
+    # getting the titles of tasks supposed to be done
      print('\t' value['title'])    
