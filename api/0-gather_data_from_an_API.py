d650922b0ccdbb67caaa15ac168dbbdb3f9d58f9
karanidenis <you@example.com>
2022-10-18 11:27:42 +0300

    getting information from an api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,38 +1,37 @@
 #!/usr/bin/python3
 
 """
-module gets information from an api
+Module
 """
 
 import requests
 import sys
 
-if __name__ == '__main__':
-    api_url = 'https://jsonplaceholder.typicode.com/todos/'.format(sys.argv[1])
-    # this is the api url used to get the list of tasks completed 
-    api_url_2 = 'https://jsonplaceholder.typicode.com/{}/'.format(sys.argv[1])
-    # this url is used to get names of employees
 
-    names_and_tasks = requests.get(api_url)
-    result = names_and_tasks.json()
-    titles = requests.get(api_url_2)
-    result_1 = titles.json()
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+        sys.argv[1])
+    url_2 = "https://jsonplaceholder.typicode.com/users/{}/".format(
+        sys.argv[1])
 
-    empl_name = result_1.get('name')
+    response = requests.get(url)
+    result = response.json()
+    response_2 = requests.get(url_2)
+    result_2 = response_2.json()
+
+    item_2 = result_2.get('name')
 
     count = 0
-    count_1 = 0
+    count_2 = 0
 
-    for value in result:
-        if value.get('userId') == int(sys.argv[1]):
-            # getting the total tasks supposed to be done
-            count_1 += 1
-        if value.get('completed') and value.get('userId') == int(sys.argv[1]):
-            # getting tasks completed
-            count += 1 
-    print('Employee', empl_name 'is done with tasks(', count_1'/'count '):')
-        
-    for value in result:
-        if value.get('completed') and value.get('userId') == int(sys.argv[1]):
-        # getting the titles of tasks supposed to be done
-             print('\t' value['title'])
+    for item in result:
+        if item.get('userId') == int(sys.argv[1]):
+            count_2 += 1
+        if item.get('completed') and item.get('userId') == int(sys.argv[1]):
+            count += 1
+    print('Employee {} is done with tasks({}/{}):'.format(item_2,
+                                                          count, count_2))
+
+    for item in result:
+        if item.get('completed') and item.get('userId') == int(sys.argv[1]):
+            print("\t {}".format(item['title']))
