06986fa24994088c22e0a13a69611dc25acec1a2
karanidenis <you@example.com>
2022-10-21 08:42:38 +0300

    APIs

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -10,35 +10,30 @@ import requests
 import sys 
 
 
-def main():
-    """main function"""
-    user_id = int(sys.argv[1])
-    todo_url = 'https://jsonplaceholder.typicode.com/todos'
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-
-    file_content = []
-
-    response = requests.get(todo_url)
-    user_name = requests.get(user_url).json().get('username')
-
-    for todo in response.json():
-        if todo.get('userId') == user_id:
-            file_content.append(
-                [str(user_id),
-                 user_name,
-                 todo.get('completed'),
-                 "{}".format(todo.get('title'))])
-
-    print(file_content)
-    file_name = "{}.csv".format(user_id)
-    with open(file_name, 'w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        for row in file_content:
-            for item in row:
-                str(item)
-            csv_writer.writerow(row)
-        print('file written successfully')
-
-
 if __name__ == "__main__":
-    main()
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    userid = sys.argv[1]
+    user = '{}users/{}'.format(url, userid)
+    res = requests.get(user)
+    json_o = res.json()
+    name = json_o.get('username')
+
+    todos = '{}todos?userId={}'.format(url, userid)
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
+    for task in tasks:
+        l_task.append([userid,
+                       name,
+                       task.get('completed'),
+                       task.get('title')])
+
+    filename = '{}.csv'.format(userid)
+    with open(filename, mode='w') as employee_file:
+        employee_writer = csv.writer(employee_file,
+                                     delimiter=',',
+                                     quotechar='"',
+                                     quoting=csv.QUOTE_ALL)
+        for task in l_task:
+            employee_writer.writerow(task)
